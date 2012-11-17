eb2bc438c4f1dc33cc4217366a2be02e0e0d244e
rkukuczka <rafal@pkey.pl>
2012-11-17 12:27:11 +0000

    Bledy ortograficzne, formatowanie

--- DlaTaty/lib/main.rb
+++ DlaTaty/lib/main.rb
@@ -1,54 +1,52 @@
 
-
 class Operacje
 
- def statr
-   puts'Witam w Kalulatorze
-   wubierz opcje:
+  def statr
+    puts'Witam w Kalulatorze
+   wybierz opcje:
 
    q - dodawanie
    w - odejmowanie
    e - mnorzenie
    r - dzielenie'
-
    
- end
-
-
-
+  end
 
   def dodawanie
-puts 'dzialaj'
- skladnik1 = gets().to_i()
- skladnik2 = gets().to_i()
+    puts 'dzialaj'
+    skladnik1 = gets().to_i()
+    skladnik2 = gets().to_i()
    
- suma = skladnik1 + skladnik2
+    suma = skladnik1 + skladnik2
     puts suma
   end
+
   def odejmowanie
     puts 'dzialaj'
- skladnik1 = gets().to_i()
- skladnik2 = gets().to_i()
+    skladnik1 = gets().to_i()
+    skladnik2 = gets().to_i()
 
- suma = skladnik1 - skladnik2
+    suma = skladnik1 - skladnik2
     puts suma
   end
+
   def mnorzene
     puts 'dzialaj'
- skladnik1 = gets().to_f()
- skladnik2 = gets().to_f()
+    skladnik1 = gets().to_f()
+    skladnik2 = gets().to_f()
 
- suma = skladnik1 * skladnik2
+    suma = skladnik1 * skladnik2
     puts suma
   end
-def dielenie
-  puts 'dzialaj'
- skladnik1 = gets().to_i()
- skladnik2 = gets().to_i()
 
- suma = skladnik1 / skladnik2
+  def dielenie
+    puts 'dzialaj'
+    skladnik1 = gets().to_i()
+    skladnik2 = gets().to_i()
+
+    suma = skladnik1 / skladnik2
     puts suma
-end
+  end
 
 end
 
@@ -56,22 +54,14 @@ ludz = Operacje.new
 ludz.statr
 
 @odp = gets().to_s().chop().downcase()
+if @odp == 'q'
+  ludz.dodawanie
+elsif @odp == 'w'
+  ludz.odejmowanie
+elsif @odp == 'e'
+  ludz.mnorzene
+elsif @odp == 'r'
+  ludz.dielenie
+else puts '???'
 
-
-   if @odp == 'q'
-     ludz.dodawanie
-
-   elsif @odp == 'w'
-     ludz.odejmowanie
-
-   elsif @odp == 'e'
-     ludz.mnorzene
-
-   elsif @odp == 'r'
-     ludz.dielenie
-
-   else puts '???'
-
-
-
- end
+end
